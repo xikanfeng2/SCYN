1a06618356f9027b5b263e740f85aed0e270958c
FENG Xikang <xikanfeng2@gmail.com>
2019-12-31 20:48:32 +0800

    add gitignore;

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="",
     packages=setuptools.find_packages(),
+    scripts=['scyn/run-scope.R'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
